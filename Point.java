cee8e6ba440148cc3e7b0560c4b3a49baa59f448
priyarganiger6 <rganigerpriya@gmail.com>
2026-09-21 09:48:22 +0530

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,20 @@
+class Point {
+    int x;
+    int y;
+
+    // Constructor
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void displayPoint() {
+        System.out.println("Point = (" + x + ", " + y + ")");
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(10, 20);
+
+        p.displayPoint();
+    }
+}
